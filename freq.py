939e2d2e80eb6e79807448692eff36b32b096491
mesargent <detroitteatime@gmail.com>
2016-06-25 16:53:26 -0700

    added student intervention project

--- freq.py
+++ freq.py
@@ -1,11 +1,10 @@
 sample_memo = '''
-Milt, we're gonna need to go ahead and move you downstairs into storage B. We have some new people coming in, and we need all the space we can get. So if you could just go ahead and pack up your stuff and move it down there, that would be terrific, OK?
+Milt, we're gonna need to go ahead and move you downstairs into storage B. We have some new people coming in, and we need all the space we can get. 
+So if you could just go ahead and pack up your stuff and move it down there, that would be terrific, OK?
 Oh, and remember: next Friday... is Hawaiian shirt day. So, you know, if you want to, go ahead and wear a Hawaiian shirt and jeans.
 Oh, oh, and I almost forgot. Ahh, I'm also gonna need you to go ahead and come in on Sunday, too...
-Hello Peter, whats happening? Ummm, I'm gonna need you to go ahead and come in tomorrow. So if you could be here around 9 that would be great, mmmk... oh oh! and I almost forgot ahh, I'm also gonna need you to go ahead and come in on Sunday too, kay. We ahh lost some people this week and ah, we sorta need to play catch up.
+Hello Peter, whats happening? Ummm, I'm gonna need you to go ahead and come in tomorrow. So if you could be here around 9 that would be great, mmmk... 
 '''
-
-#
 #   Maximum Likelihood Hypothesis
 #
 #
